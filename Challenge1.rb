54744fc3d4bc82963be1e85cd0bd99d27d9002d7
chunchiar <chunchiar_7@hotmail.com>
2016-07-29 20:02:24 +0800

    Add files via upload

---
+++ Challenge1.rb
@@ -0,0 +1,7 @@
+counter = 1
+ until counter == 100
+  puts counter
+  counter += 1
+ end
+
+puts counter
---
+++ Challenge2.rb
@@ -0,0 +1,11 @@
+1.upto(100) do |i|
+ if i % 3 == 0 and i % 5 == 0
+  puts "OlaBola"
+elsif i % 3 == 0
+  puts "Ola"
+elsif i % 5 == 0
+  puts "Bola"
+ else
+  puts i
+  end
+end
